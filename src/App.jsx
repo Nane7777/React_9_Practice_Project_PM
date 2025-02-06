e0c5545b01328fd6437be92b0041583118cf2d1a
Nicolas Gemine <gemine_nicolas@hotmail.com>
2025-02-06 16:50:37 +0100

    06-02-25 React 9-155

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
+
+function App() {
+
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            };
+        });
+    }
+
+    let content;
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject />;
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+    }
+
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectSidebar onStartAddProject={handleStartAddProject} />
+            {content}
+        </main>
+    );
+}
+
+export default App;
